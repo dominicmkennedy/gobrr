5e89328be1c99ec4e8e66c5ae4fc92cbf382f1f1
dominicmkennedy <dominicmkennedy@gmail.com>
2022-01-15 01:09:28 -0500

    added testing for all functions

--- gobrr_test.go
+++ gobrr_test.go
@@ -2,38 +2,105 @@ package gobrr
 
 import (
 	"bytes"
+	"io"
 	"testing"
 )
 
 func TestCreateEmptyMemfile(t *testing.T) {
-    file, err := CreateEmptyMemfile()
-    if err != nil {
-        t.Errorf("file creation failed: %s\n", err)
-    }
+	file, err := CreateEmptyMemfile()
+	if err != nil {
+		t.Errorf("file creation failed: %s\n", err)
+	}
 
-    stat, err := file.Stat()
-    if err != nil {
-        t.Errorf("could not retrive file stats: %s\n", err)
-    }
+	stat, err := file.Stat()
+	if err != nil {
+		t.Errorf("could not retrive file stats: %s\n", err)
+	}
 
-    if stat.Size() != 0 {
-        t.Errorf("file size is not zero\n")
-    }
+	if stat.Size() != 0 {
+		t.Errorf("file size is not zero\n")
+	}
 }
 
 func TestCreateMemfileFromData(t *testing.T) {
-    data := []byte("abcdefghijklmnopqrstuvwxyz")
+	data := []byte("abcdefghijklmnopqrstuvwxyz")
+	buf := make([]byte, len(data))
 
-    file, err := CreateMemfileFromData(data)
-    if err != nil {
-        t.Errorf("file creation failed: %s\n", err)
-    }
+	file, err := CreateMemfileFromData(data)
+	if err != nil {
+		t.Errorf("file creation failed: %s\n", err)
+	}
 
-    buf := make([]byte, len(data))
+	if _, err := file.Read(buf); err != io.EOF && err != nil {
+		t.Errorf("error reading file: %v\n", err)
+	}
 
-    file.Read(buf)
+	if !bytes.Equal(data, buf) {
+		t.Errorf("file did not contain the proper contents\nexpected: %v\nreceived: %v\n", data, buf)
+	}
+}
 
-    if !bytes.Equal(data, buf) {
-        t.Errorf("file did not contain the proper contents")
-    }
+func TestCopyFileToMemfile(t *testing.T) {
+	data := []byte("abcdefghijklmnopqrstuvwxyz")
+	buf := make([]byte, len(data))
+
+	oldFile, err := CreateMemfileFromData(data)
+	if err != nil {
+		t.Errorf("file creation failed: %s\n", err)
+	}
+
+	file, err := CopyFileToMemfile(oldFile)
+	if err != nil {
+		t.Errorf("error copying to memfile: %v\n", err)
+	}
+
+	if _, err := file.Read(buf); err != io.EOF && err != nil {
+		t.Errorf("error reading file: %v\n", err)
+	}
+
+	if !bytes.Equal(data, buf) {
+		t.Errorf("file did not contain the proper contents\nexpected: %v\nreceived: %v\n", data, buf)
+	}
+}
+
+//TODO
+func TestCopyFilePathToMemfile(t *testing.T) {
+	data := []byte("abcdefghijklmnopqrstuvwxyz")
+	buf := make([]byte, len(data))
+
+	oldFile, err := CreateMemfileFromData(data)
+	if err != nil {
+		t.Errorf("file creation failed: %s\n", err)
+	}
+
+	file, err := CopyFilePathToMemfile(oldFile.Name())
+	if err != nil {
+		t.Errorf("error copying to memfile: %v\n", err)
+	}
+
+	if _, err := file.Read(buf); err != io.EOF && err != nil {
+		t.Errorf("error reading file: %v\n", err)
+	}
+
+	if !bytes.Equal(data, buf) {
+		t.Errorf("file did not contain the proper contents\nexpected: %v\nreceived: %v\n", data, buf)
+	}
+}
+
+func TestCopyMemfileToBytes(t *testing.T) {
+	data := []byte("abcdefghijklmnopqrstuvwxyz")
+
+	file, err := CreateMemfileFromData(data)
+	if err != nil {
+		t.Errorf("file creation failed: %s\n", err)
+	}
+
+	buf, err := CopyMemfileToBytes(file)
+	if err != nil {
+		t.Errorf("error copying mem file to bytes: %v\n", err)
+	}
+
+	if !bytes.Equal(data, buf) {
+		t.Errorf("file did not contain the proper contents\nexpected: %v\nreceived: %v\n", data, buf)
+	}
 }
